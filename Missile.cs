2f56143324e12801b9230b1f74a1cb644389452c
huanitosus <167616624+huanitosus@users.noreply.github.com>
2024-12-29 19:51:30 +0300

    Create Missile.cs

---
+++ Missile.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Missile : MonoBehaviour
+{
+    public float speed;
+ 
+    void Update()
+    {
+        Destroy(gameObject, 3);
+
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+ 
+    void OnTriggerEnter(Collider other) {
+
+        Enemy enemy = other.GetComponent<Enemy>();
+        Destroy(enemy.gameObject);
+
+        Destroy(gameObject);
+ 
+    }
+ 
+}
